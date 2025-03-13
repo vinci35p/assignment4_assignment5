4db582f2249cbba9f4e6a10ec794b5b5bd2796f2
Vincent35p <vinsoyt4321@gmail.com>
2025-03-14 00:24:43 +0800

    created a pseudocode for batch4_prog03

---
+++ batch4_prog03.py
@@ -0,0 +1,7 @@
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number.
+
+# Input a number.
+
+# Determine the highest number.
+
+# Print the highest number.
