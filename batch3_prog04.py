5c74c54a43ea816b840c1d1373c50e922ae3bdcb
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 22:59:40 +0800

    created a pseudocode for batch3_prog04

---
+++ batch3_prog04.py
@@ -0,0 +1,7 @@
+# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number.
+
+# Input a number and initialize the list.
+
+# Print invalid entry if the inputted value is not a number.
+
+# Determine the lowest number from the inputted numbers.
