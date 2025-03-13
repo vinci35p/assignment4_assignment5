d8514101e801d202aa567a28fce272c412565b3d
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 18:57:31 +0800

    created pseudocode for batch1_prog10

---
+++ batch1_prog10.py
@@ -0,0 +1,7 @@
+# Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+# Output: All the numbers from 1-100 except numbers ending in zero.
+
+# Determine the numbers ending with zero, and exclude them.
+
+# Print the included numbers.
