dc8c3bdcd5da7da16fab6b770de6a4e96ec6e0cb
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 17:38:43 +0800

    created input functions

--- batch1_prog04.py
+++ batch1_prog04.py
@@ -1,5 +1,7 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
 # Input two numbers.
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
 
 # Print the product of 1st number and the 2nd number.
