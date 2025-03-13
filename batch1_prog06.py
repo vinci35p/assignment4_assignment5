c58beb65a99d4df280739f1b7a10f9e3b8e25c48
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 18:00:16 +0800

    added raise function inside the print statement

--- batch1_prog06.py
+++ batch1_prog06.py
@@ -5,3 +5,4 @@ num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
 # Print the result when the 1st number is raised to the value of the 2nd number.
+print(f"The result is {num1 ** num2}.")
