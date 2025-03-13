2fc7d5c8cc104c0286c67b667d2f63d9eb15eb3b
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 20:35:30 +0800

    created pseudocode for batch2_prog06

---
+++ batch2_prog06.py
@@ -0,0 +1,7 @@
+# Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+# Initialize number list variable.
+
+# Input 10 numbers.
+
+# Print the first number minus all the remaining numbers.
