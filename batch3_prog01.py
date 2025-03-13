5311bdef623d06c8aad72b4e03aff6677ea07c58
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 22:05:28 +0800

    tried loop and variable listing but not working

--- batch3_prog01.py
+++ batch3_prog01.py
@@ -1,7 +1,11 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
 # Input 10 numbers.
+for i in range(10):
+    num1 = [int(input(f"Enter number {i + 1}: "))]
 
 # Determine numbers who don't have a duplicate.
+    lone_numbers = [num for num in num1 if num1.count(num) == 1]
 
 # Print the lone numbers.
+    print(lone_numbers)
