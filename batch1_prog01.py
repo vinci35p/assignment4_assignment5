7f746d0cbec8f2729cde02f8e1fb52c30ccafdb5
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 16:47:34 +0800

    created input function

---
+++ batch1_prog01.py
@@ -0,0 +1,4 @@
+#Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+#Input function and variable
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
