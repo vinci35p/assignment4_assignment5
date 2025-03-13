1d65aa7920e473f68c58cb4ec1961a0d242f480b
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 21:15:28 +0800

    edited the margin, and added a print statement

--- batch2_prog08.py
+++ batch2_prog08.py
@@ -3,9 +3,11 @@
 # Output: All odd numbers from 1 to 100.
 print("All odd numbers from 1-100: ")
 
-# While less than a hundred one and the remainder when divided by two is not equal to zero include the number.
+# While less than a hundred and the remainder when divided by two is not equal to zero include the number.
 num = 1
-while num <= 101:
+while num <= 100:
     if num % 2 != 0:
 
 # Print all included numbers.
+        print(num)
+        num += 2
