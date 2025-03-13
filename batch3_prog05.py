3d2f33c34bfbad954b678e70c6442ec5825d5914
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 23:24:36 +0800

    created a pseudocode for batch3_prog05

---
+++ batch3_prog05.py
@@ -0,0 +1,7 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort()if function:
+
+# Input a number.
+
+# Determine the numbers from lowest to highest.
+
+# Print the numbers from lowest to highest.
