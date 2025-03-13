c35f949463e3889eb18e05c2c2ae0b500e73c8c9
Vincent35p <vinsoyt4321@gmail.com>
2025-03-14 00:37:09 +0800

    referenced prog05 batch3 with variable list init, loop input func appending it to variable list

--- batch4_prog04.py
+++ batch4_prog04.py
@@ -1,6 +1,12 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function:
 
 # Input a number.
+num_list = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        num_list.append(num)
 
 # Determine numbers from highest to lowest.
 
