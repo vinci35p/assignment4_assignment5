56afc8c7182192dad6189361aaf9398d58c0e80c
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 20:19:57 +0800

    created pseudocode for batch2_prog05

---
+++ batch2_prog05.py
@@ -0,0 +1,6 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+# Input two numbers.
+
+# Print the remainder when the 1st number is divided by the 2nd number.
+
