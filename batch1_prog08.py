f7afe78c5f1f0d59932ede4fefa8e4342ecb85ca
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 18:29:37 +0800

    added the variable storing sum of loop of input function and if function to count odd numbers

--- batch1_prog08.py
+++ batch1_prog08.py
@@ -1,5 +1,6 @@
 # Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
 # Input 10 numbers, count odd numbers.
+odd_numbers = sum(1 for i in range(10) if int(input(f"Enter number {i + 1}: ")) % 2 != 0)
 
 # Print the counted odd numbers for the given input numbers.
