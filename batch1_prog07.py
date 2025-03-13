7ad3c1bd9596b32eed3d29f92abb074b89878552
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 18:11:44 +0800

    removed the initialization variable

--- batch1_prog07.py
+++ batch1_prog07.py
@@ -1,7 +1,5 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
-# Initialize the variable.
-
 # Sum, input and loop function stored in the variable.
 
 # Print the resultant.
