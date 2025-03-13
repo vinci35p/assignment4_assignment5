c7a2cd6926d443002f93ad404c190489145c5e38
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 22:19:59 +0800

    adjusted variable list to count every duplicate and exclude them by using if statement

--- batch3_prog01.py
+++ batch3_prog01.py
@@ -1,11 +1,10 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
 # Input 10 numbers.
-for i in range(10):
-    num1 = [int(input(f"Enter number {i + 1}: "))]
+num_list = [int(input(f"Enter number {i + 1}: ")) for i in range(10)]
 
 # Determine numbers who don't have a duplicate.
-    lone_numbers = [num for num in num1 if num1.count(num) == 1]
+lone_numbers = [num for num in num_list if num_list.count(num) == 1]
 
 # Print the lone numbers.
-    print(lone_numbers)
+print(f"The lone numbers are: {lone_numbers}.")
