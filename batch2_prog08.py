83402dfec904c649bd1cb5b4a5cb5c47979b9e68
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 21:13:30 +0800

    added variable, and if statement inside a while loop

--- batch2_prog08.py
+++ batch2_prog08.py
@@ -1,7 +1,11 @@
 # Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
 # Output: All odd numbers from 1 to 100.
+print("All odd numbers from 1-100: ")
 
-# While less than a hundred and the remainder when divided by two is not equal to zero include the number.
+# While less than a hundred one and the remainder when divided by two is not equal to zero include the number.
+num = 1
+while num <= 101:
+    if num % 2 != 0:
 
 # Print all included numbers.
