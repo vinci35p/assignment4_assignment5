bd8bae409fd1d9b576c9970f4037a006613b77be
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 20:44:09 +0800

    added new variable to subtract the 1st number to remaining, and print statement

--- batch2_prog06.py
+++ batch2_prog06.py
@@ -8,4 +8,8 @@ for num in range(10):
     num1 = int(input(f"Enter number {num + 1}: "))
     numbers.append(num1)
 
+#Subtract the 1st number listed, then the rest of the remaining.
+result = numbers[0] - sum(numbers[1:])
+
 # Print the first number minus all the remaining numbers.
+print(f"The result is {result}.")
