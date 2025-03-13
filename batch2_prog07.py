40777f4241fd8c5d854dfcbc626074b944d1d8a5
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 20:54:33 +0800

    edited pseudocode, added variable consists of the sum of every even numbers inputted

--- batch2_prog07.py
+++ batch2_prog07.py
@@ -1,7 +1,6 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
 
-# Input 10 numbers.
-
-# Count the even numbers.
+# Input 10 numbers, and count even numbers.
+even_numbers = sum(1 for i in range(10) if int(input(f"Enter number {i + 1}: ")) % 2 == 0)
 
 # Print the count amount.
