46f4a43b3ce624ab0da0d1cef562a2c6872f82ad
Vincent35p <vinsoyt4321@gmail.com>
2025-03-14 00:26:13 +0800

    init the variable to none, and loop for input func

--- batch4_prog03.py
+++ batch4_prog03.py
@@ -1,6 +1,11 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number.
 
 # Input a number.
+highest_number = None
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
 
 # Determine the highest number.
 
