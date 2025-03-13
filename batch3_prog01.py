8c0fd83506992ee9d0feed7675efaa43b608ba1e
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 21:49:58 +0800

    created pseudocode for batch3_prog01

---
+++ batch3_prog01.py
@@ -0,0 +1,7 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+# Input 10 numbers.
+
+# Determine numbers who don't have a duplicate.
+
+# Print the lone numbers.
