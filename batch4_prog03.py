b2a802dd9a04d61e8ab8f52ef5c44fd77cea95b0
Vincent35p <vinsoyt4321@gmail.com>
2025-03-14 00:30:20 +0800

    added except function with if else statement just like batch3prog4

--- batch4_prog03.py
+++ batch4_prog03.py
@@ -7,9 +7,14 @@ while True:
     try:
         num = int(input("Enter a number: "))
 
+# Determine the highest number.
         if highest_number is None or num > highest_number:
             highest_number = num
 
-# Determine the highest number.
-
 # Print the highest number.
+    except ValueError:
+        if highest_number is not None:
+            print(f"The highest number is {highest_number}.")
+        else:
+            print("No valid numbers are inputted.")
+        break
