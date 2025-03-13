6ee97da959af73a699b4baab82907d2848a19863
Vincent35p <vinsoyt4321@gmail.com>
2025-03-14 00:46:55 +0800

    added loop for input func, and variable list

--- batch4_prog05.py
+++ batch4_prog05.py
@@ -1,6 +1,14 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
 # Input a number.
+num_list = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+
+    except ValueError:
+        break
 
 # Determine the average.
 
