4b48640ca6d86ed4a53f38b065d94361d9b2d08d
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 20:16:56 +0800

    added print statement consisting of quotient operation

--- batch2_prog04.py
+++ batch2_prog04.py
@@ -5,3 +5,4 @@ num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
 # Print the quotient of two numbers without decimal.
+print(f"The quotient is {int(num1 / num2)}.")
