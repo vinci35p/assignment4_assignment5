c00e634fdc2f845a556d9ee7216a352fabb49838
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 16:55:53 +0800

    using f string to print function and use max function to deter bigger number

--- batch1_prog01.py
+++ batch1_prog01.py
@@ -5,4 +5,6 @@
 num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
-# Print function with max function to deter bigger number.
+# Using f string to the print function with max function to deter bigger number.
+
+print(f"The bigger number is {max(num1, num2)}.")
