46e6e37fa50db30cb6c7609eb5134a9dcfa297af
Vincent35p <vinsoyt4321@gmail.com>
2025-03-14 00:13:48 +0800

    added if statement to assert frequency, and for func to count, added print statement. not working

--- batch4_prog02.py
+++ batch4_prog02.py
@@ -6,10 +6,23 @@ num_list = []
 while True:
     try:
         num = int(input("Enter a number: "))
+        num_list.append(num)
 
     except ValueError:
         break
 
 # Determine the most iterated number inputted.
+if not num_list:
+    print("No numbers are entered.")
+else:
+    iteration = {}
+    for num in num_list:
+        if num in iteration:
+            iteration[num] += 1
+
+    most_iterated_num = max(iteration, key=iteration.get)
+    most_count = iteration[most_iterated_num]
 
 # Print the number.
+
+    print(most_iterated_num)
