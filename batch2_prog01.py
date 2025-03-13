8d8be06cd1928bd4e35731045de4f11023f47f74
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 19:57:02 +0800

    added input function

--- batch2_prog01.py
+++ batch2_prog01.py
@@ -1,5 +1,7 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 
 # Input two numbers.
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
 
 # Print the minimum number between two numbers.
