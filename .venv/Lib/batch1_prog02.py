cdaa66ee0e8355149a4b98ed2408c5f5b034aafb
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 17:12:11 +0800

    created if and else function

--- .venv/Lib/batch1_prog02.py
+++ .venv/Lib/batch1_prog02.py
@@ -5,5 +5,9 @@ num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
 # If the first number and the second number has the same value print Equal.
+if num1 == num2:
+    print("Equal.")
 
 # Or else print the max number.
+else:
+    print(f"The bigger number is {max(num1, num2)}.")
