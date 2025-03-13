3f91b9b0c7728fe91888fb856ce2f0750ae0cfd1
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 23:30:25 +0800

    init variable storing empty list, added loop, input and except func inhibiting if statement with sort func

--- batch3_prog05.py
+++ batch3_prog05.py
@@ -1,7 +1,17 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort()if function:
 
 # Input a number.
+num_list = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        num_list.append(num)
+
 
 # Determine the numbers from lowest to highest.
+    except ValueError:
+        if num_list:
+            num_list.sort()
 
 # Print the numbers from lowest to highest.
