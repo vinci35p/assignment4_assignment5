92f469770c1060d4f01aaf5e985f610b2eec5daf
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 21:22:33 +0800

    created pseudocode for batch2_prog09

---
+++ batch2_prog09.py
@@ -0,0 +1,7 @@
+# Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
+# Output: Numbers from 0-100 that doesn't end with 0 and 5.
+
+# Include all numbers not ending with zero or 5.
+
+# Print the included numbers.
