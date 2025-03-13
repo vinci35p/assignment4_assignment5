e5cff231ae3281372d2d36e76e021171992291f5
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 20:24:56 +0800

    added if else and print statement

--- batch2_prog05.py
+++ batch2_prog05.py
@@ -4,5 +4,10 @@
 num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
-# Print the remainder when the 1st number is divided by the 2nd number.
+# If the 2nd number is equal to zero, output: not allowed.
+if num2 == 0:
+    print("Numbers divided by zero is undefined.")
 
+# Print the remainder when the 1st number is divided by the 2nd number.
+else:
+    print(f"The remainder is {num1 % num2}.")
