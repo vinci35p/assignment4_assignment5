8048484243b86919cba11a4f94ad3748b6f48501
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 17:55:46 +0800

    created pseudocode for batch1_prog06

---
+++ batch1_prog06.py
@@ -0,0 +1,5 @@
+# Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+# Input two numbers.
+
+# Print the result when the 1st number is raised to the value of the 2nd number.
