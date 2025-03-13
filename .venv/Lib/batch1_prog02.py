2672d3f485be120145fe381a780c9231a928fa18
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 17:04:34 +0800

    created pseudocode for batch1_prog02

---
+++ .venv/Lib/batch1_prog02.py
@@ -0,0 +1,5 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+# Input two numbers.
+
+# Print equal when first number has the same value with second number.
