bc01d0ea5fe181a3f7940b980bea10b8bb9bd0e6
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 21:36:25 +0800

    adding the usual input function

--- batch2_prog10.py
+++ batch2_prog10.py
@@ -1,6 +1,8 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
 # Input two numbers.
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
 
 # Determine the numbers between two inputted two numbers.
 
