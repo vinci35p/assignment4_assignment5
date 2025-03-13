0d0282e89c441a1f8f61fa309b604b4043348b26
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 20:21:24 +0800

    added input function

--- batch2_prog05.py
+++ batch2_prog05.py
@@ -1,6 +1,8 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 
 # Input two numbers.
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
 
 # Print the remainder when the 1st number is divided by the 2nd number.
 
