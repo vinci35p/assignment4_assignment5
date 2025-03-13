1949595d3f65b7e532a803f9660c80e18cfe366f
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 18:14:06 +0800

    added the variable function consisting the sum, input and loop functions

--- batch1_prog07.py
+++ batch1_prog07.py
@@ -1,6 +1,7 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
 # Sum, input and loop function stored in the variable.
+total = sum(int(input(f"Enter number {i + 1}: ")) for i in range(10))
 
 # Print the resultant.
 
