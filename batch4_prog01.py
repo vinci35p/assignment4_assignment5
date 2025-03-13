54c72d8db87ff75517c66bc41475ba7a6a267933
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 23:39:19 +0800

    added input func stored in a variable list

--- batch4_prog01.py
+++ batch4_prog01.py
@@ -1,6 +1,7 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
 # Input 10 numbers.
+num_list = [int(input(f"Enter number {i + 1}: ")) for i in range(10)]
 
 # Determine the numbers that have duplicate.
 
