6309ba36d756e65a1316a3b9a7d0635a78535fa8
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 18:10:10 +0800

    created pseudocode for batch1_prog07

---
+++ batch1_prog07.py
@@ -0,0 +1,8 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+# Initialize the variable.
+
+# Sum, input and loop function stored in the variable.
+
+# Print the resultant.
+
