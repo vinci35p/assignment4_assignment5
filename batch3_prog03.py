be58223e0a6e59656d7ff4f69b14334ea1e22b5d
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 22:38:26 +0800

    created a pseudocode for batch3_prog03

---
+++ batch3_prog03.py
@@ -0,0 +1,11 @@
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+
+# Input a number.
+
+# Determine if number is invalid, duplicate or unique.
+
+# Print if number is invalid.
+
+# Print if number is duplicate.
+
+# Print if number is unique.
