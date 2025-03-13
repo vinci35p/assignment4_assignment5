18a5e4e7669aa0ad817e6d313e8cb9943757ab3b
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 17:09:45 +0800

    edited pseudocode with if statement, and created the input function

--- .venv/Lib/batch1_prog02.py
+++ .venv/Lib/batch1_prog02.py
@@ -1,5 +1,9 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 
 # Input two numbers.
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
 
-# Print equal when first number has the same value with second number.
+# If the first number and the second number has the same value print Equal.
+
+# Or else print the max number.
