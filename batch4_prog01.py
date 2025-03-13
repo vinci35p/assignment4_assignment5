671d681534e75eb9095d485385ec4b5055a26ec7
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 23:38:12 +0800

    created pseudocode for batch4_prog01

---
+++ batch4_prog01.py
@@ -0,0 +1,8 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+# Input 10 numbers.
+
+# Determine the numbers that have duplicate.
+
+# Print the numbers.
+
