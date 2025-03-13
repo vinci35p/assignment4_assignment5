afa0f8e62549844129852f5d70d2bf9671183f52
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 20:14:03 +0800

    created pseudocode for batch2_prog04

---
+++ batch2_prog04.py
@@ -0,0 +1,5 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point.
+
+# Input two numbers.
+
+# Print the quotient of two numbers without decimal.
