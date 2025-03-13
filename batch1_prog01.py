8bc3394b3bc6eff0ee9cacb281cb06599b68324e
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 16:52:16 +0800

    added space and period on comments to prog01_batch1 to follow rules

--- batch1_prog01.py
+++ batch1_prog01.py
@@ -1,4 +1,8 @@
-#Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
-#Input function and variable
+# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+# Input function and variable.
+
 num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
+
+# Print function with max function to deter bigger number.
