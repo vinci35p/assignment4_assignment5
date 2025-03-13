7f4d10ab79957b8d7d07a206787495483e6a6c26
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 17:18:23 +0800

    added input function

--- .venv/batch1_prog03.py
+++ .venv/batch1_prog03.py
@@ -1,5 +1,7 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
 # Input two numbers.
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
 
 # Print the sum of the 1st number and the 2nd number.
