94306afaf5626ebd4459bdfae1f4af8b5c1bd62f
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 22:34:06 +0800

    added a set in the input section to unacknowledge duplicates, and print statement

--- batch3_prog02.py
+++ batch3_prog02.py
@@ -1,8 +1,7 @@
 # Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
 # Input 10 numbers.
-num_list = [int(input(f"Enter number {i + 1}: ")) for i in range(10)]
-
-# Determine the duplicates and exclude extras.
+num_list = {int(input(f"Enter number {i + 1}: ")) for i in range(10)}
 
 # Print the numbers.
+print(f"The numbers are: {num_list}")
