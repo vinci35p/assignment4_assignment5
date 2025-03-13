fc6d3c8a7dcb0280fdbf6c95d402995493ef7601
Vincent35p <vinsoyt4321@gmail.com>
2025-03-14 00:05:59 +0800

    added loop and except func, init num list

--- batch4_prog02.py
+++ batch4_prog02.py
@@ -1,6 +1,14 @@
 # Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 
 # Input a number.
+num_list = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+
+    except ValueError:
+        break
 
 # Determine the most iterated number inputted.
 
