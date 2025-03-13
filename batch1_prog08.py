73becb756dfd5d2ddf2efaec0d0ea31bb9a38c49
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 18:21:45 +0800

    created pseudocode for batch1_prog08

---
+++ batch1_prog08.py
@@ -0,0 +1,5 @@
+# Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+# Input 10 numbers, count odd numbers.
+
+# Print the counted odd numbers for the given input numbers.
