f32aca1879d89f7c14656d6126d11860180ac7a9
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 20:03:08 +0800

    added input function

--- batch2_prog02.py
+++ batch2_prog02.py
@@ -1,6 +1,8 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 
 # Input two numbers.
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
 
 # If the 1st number is not equal to 2nd number, print not equal.
 
