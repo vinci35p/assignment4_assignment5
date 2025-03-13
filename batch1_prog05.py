ebf9d42a8ae04737c075c269cadb1bb093d96102
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 17:43:54 +0800

    created pseudocode for batch1_prog05

---
+++ batch1_prog05.py
@@ -0,0 +1,6 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point.
+
+# Input two numbers.
+
+# Print the quotient of the 1st number and the second number with decimal point.
+
