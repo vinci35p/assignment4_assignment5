bcaed00efd652de2dbdbd8fb618c2bb3aedbcebc
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 20:05:03 +0800

    created if else statement

--- batch2_prog02.py
+++ batch2_prog02.py
@@ -5,5 +5,9 @@ num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
 # If the 1st number is not equal to 2nd number, print not equal.
+if num1 != num2:
+    print("Not equal.")
 
 # Else, print the smaller number between two numbers.
+else:
+    print(f"The smaller number is {min(num1, num2)}.")
