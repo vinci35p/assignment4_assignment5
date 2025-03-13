5e3901fc3e9803c70aba0f60a94c54c54cc7a51f
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 22:53:22 +0800

    edited the loop and adding append for list to indicate duplicates

--- batch3_prog03.py
+++ batch3_prog03.py
@@ -1,11 +1,18 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 
 # Input a number.
+number = []
 while True:
-    number = int(input("Enter a number: "))
+    try:
+        num = int(input("Enter a number: "))
 
 # Print if number is duplicate.
+        if num in number:
+            print("Duplicate.")
 
 # Print if number is unique.
+        else:
+            print("Unique")
+            number.append(num)
 
-# Print if number is invalid.
+# Print if number is invalid then end the program.
