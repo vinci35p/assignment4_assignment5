bedd50ba5590735a9d784c5b16fce795aec49e61
Vincent35p <vinsoyt4321@gmail.com>
2025-03-14 00:44:17 +0800

    created a pseudocode for batch4_prog05

---
+++ batch4_prog05.py
@@ -0,0 +1,7 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+# Input a number.
+
+# Determine the average.
+
+# Print the average.
