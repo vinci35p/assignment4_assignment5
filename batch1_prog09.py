21bdfda596c66978864410e9211e7906aeae21c3
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 18:49:30 +0800

    added loop, 2 steps from 0 to deter even numbers, and added print statement to remind what's the output

--- batch1_prog09.py
+++ batch1_prog09.py
@@ -1,6 +1,9 @@
 # Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 
-# Determine the even numbers from 1-100.
+# Output: All even numbers from 1-100.
+print("All even numbers from 1-100: ")
 
+# Determine the even numbers from 1-100.
+for num in range(0, 100, 2):
 
 # Print all the even numbers.
