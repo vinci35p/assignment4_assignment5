1fe075a673be8c94c2548dbf75df4233a01148dd
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 23:54:29 +0800

    created a pseudocode for batch4_prog02

---
+++ batch4_prog02.py
@@ -0,0 +1,7 @@
+# Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
+# Input a number.
+
+# Determine the most iterated number inputted.
+
+# Print the number.
