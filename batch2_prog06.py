939a3c6040a28280b31e90b62325a1cfd573c46c
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 20:40:15 +0800

    init the list to store the input, and added loop for input with append function

--- batch2_prog06.py
+++ batch2_prog06.py
@@ -1,7 +1,11 @@
 # Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
 # Initialize number list variable.
+numbers = []
 
 # Input 10 numbers.
+for num in range(10):
+    num1 = int(input(f"Enter number {num + 1}: "))
+    numbers.append(num1)
 
 # Print the first number minus all the remaining numbers.
