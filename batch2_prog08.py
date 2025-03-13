b65faa76ee034743129d5ab3996fe098e4030732
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 21:04:08 +0800

    created pseudocode for batch2_prog08

---
+++ batch2_prog08.py
@@ -0,0 +1,7 @@
+# Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+# Output: All odd numbers from 1 to 100.
+
+# While less than a hundred and the remainder when divided by two is not equal to zero include the number.
+
+# Print all included numbers.
