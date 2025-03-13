1436a08ceb2621f7e407de5cc0ee32c0194501a5
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 23:48:14 +0800

    referenced to prog01 batch 3, replaced equal with greater than 1 to modify duplicates

--- batch4_prog01.py
+++ batch4_prog01.py
@@ -4,6 +4,7 @@
 num_list = [int(input(f"Enter number {i + 1}: ")) for i in range(10)]
 
 # Determine the numbers that have duplicate.
+duplicate_num = {num for num in num_list if num_list.count(num) > 1}
 
 # Print the numbers.
 
