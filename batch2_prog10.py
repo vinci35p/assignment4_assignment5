31dd4949db9a4e6eee18341ac0c56d1bef2b0ce3
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 21:35:11 +0800

    created pseudocode for batch2_prog10

---
+++ batch2_prog10.py
@@ -0,0 +1,7 @@
+#Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+# Input two numbers.
+
+# Determine the numbers between two inputted two numbers.
+
+# Print the determined numbers.
