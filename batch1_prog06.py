86a862421a0cc30fe72abf25e798d48efe05c3db
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 17:56:51 +0800

    added input function

--- batch1_prog06.py
+++ batch1_prog06.py
@@ -1,5 +1,7 @@
 # Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
 # Input two numbers.
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
 
 # Print the result when the 1st number is raised to the value of the 2nd number.
