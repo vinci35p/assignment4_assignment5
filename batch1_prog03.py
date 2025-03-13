50f84d86df8a554ae82a35bbfc2ff850fe999033
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 17:50:31 +0800

    edited the operator function in prog 03-05 to follow pep

--- batch1_prog03.py
+++ batch1_prog03.py
@@ -5,4 +5,4 @@ num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
 # Print the sum of the 1st number and the 2nd number.
-print(f"The sum is {num1+num2}.")
+print(f"The sum is {num1 + num2}.")
--- batch1_prog04.py
+++ batch1_prog04.py
@@ -5,4 +5,4 @@ num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
 # Print the product of 1st number and the 2nd number.
-print(f"The product is {num1*num2}.")
+print(f"The product is {num1 * num2}.")
