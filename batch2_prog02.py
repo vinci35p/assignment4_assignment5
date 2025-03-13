0c2c713c135ce3d7ded84b2cc7e519505e666cf1
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 20:01:28 +0800

    created pseudocode for batch2_prog02

---
+++ batch2_prog02.py
@@ -0,0 +1,7 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+# Input two numbers.
+
+# If the 1st number is not equal to 2nd number, print not equal.
+
+# Else, print the smaller number between two numbers.
