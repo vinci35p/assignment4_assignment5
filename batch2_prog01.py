e87048ea6c5747829f0a0caf7ed50a4e58774656
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 19:58:33 +0800

    added print statement with min function inside to deter the smaller number

--- batch2_prog01.py
+++ batch2_prog01.py
@@ -5,3 +5,4 @@ num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
 # Print the minimum number between two numbers.
+print(f"The smaller number is {min(num1, num2)}.")
