1ba4d0984ff90b6c26d34902314a79063268159e
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 18:30:44 +0800

    added the print statement with the count of odd_numbers

--- batch1_prog08.py
+++ batch1_prog08.py
@@ -4,3 +4,4 @@
 odd_numbers = sum(1 for i in range(10) if int(input(f"Enter number {i + 1}: ")) % 2 != 0)
 
 # Print the counted odd numbers for the given input numbers.
+print(f"The number of odd numbers are {odd_numbers}.")
