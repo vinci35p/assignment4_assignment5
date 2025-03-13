a5d2837d1723cedd0dc2925f3578549fca7e0997
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 17:51:40 +0800

    added format specifier in prog05 to just print the decimal point

--- batch1_prog05.py
+++ batch1_prog05.py
@@ -5,5 +5,5 @@ num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
 # Print the quotient of the 1st number and the second number with decimal point.
-print(f"The quotient is {num1 / num2}.")
+print(f"The quotient is {num1 / num2:.0f}.")
 
