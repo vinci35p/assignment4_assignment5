963f85702b8a7833f4f1c3815e5971169a0a3c0f
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 21:31:18 +0800

    added loop that exclude numbers divisible by 5

--- batch2_prog09.py
+++ batch2_prog09.py
@@ -1,7 +1,10 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
 # Output: Numbers from 0-100 that doesn't end with 0 and 5.
+print("All numbers from 1-100 that does not end with 0 and 5: ")
 
 # Include all numbers not ending with zero or 5.
+for num in range(100):
+    if num % 5 != 0:
 
 # Print the included numbers.
