36ad6f0aa29d31c86b549316b16fa2480cfabf55
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 23:03:32 +0800

    added init and input function just lie=ke the recent codes

--- batch3_prog04.py
+++ batch3_prog04.py
@@ -1,6 +1,10 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number.
 
 # Input a number and initialize the list.
+numbers = []
+while True:
+    try:
+        num1 = int(input("Enter a number: "))
 
 # Print invalid entry if the inputted value is not a number.
 
