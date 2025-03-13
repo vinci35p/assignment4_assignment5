0880bfae49ea13a0817feab827ae4bfb3a383f89
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 20:50:37 +0800

    edited variable name, again to follow pep style guide

--- batch2_prog06.py
+++ batch2_prog06.py
@@ -1,15 +1,15 @@
 # Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
 # Initialize number list variable.
-numbers_list = []
+number_list = []
 
 # Input 10 numbers.
 for num in range(10):
     num1 = int(input(f"Enter number {num + 1}: "))
-    numbers.append(num1)
+    number_list.append(num1)
 
 #Subtract the 1st number listed, then the rest of the remaining.
-result = numbers[0] - sum(numbers[1:])
+result = number_list[0] - sum(number_list[1:])
 
 # Print the first number minus all the remaining numbers.
 print(f"The result is {result}.")
