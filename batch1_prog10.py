363d4f4ea199cf755a3fb74c6dbad691ce86bc16
Vincent35p <vinsoyt4321@gmail.com>
2025-03-13 19:00:22 +0800

    added loop function range and if statement under to deter numbers ending with zeros

--- batch1_prog10.py
+++ batch1_prog10.py
@@ -1,7 +1,10 @@
 # Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 
 # Output: All the numbers from 1-100 except numbers ending in zero.
+print("All numbers from 1-100 that doesn't end with zeros: ")
 
 # Determine the numbers ending with zero, and exclude them.
+for num in range(0, 100):
+    if num % 10 != 0:
 
 # Print the included numbers.
