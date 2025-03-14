e9ba92944c9153af9cda130b1eec372d3203c339
Vincent35p <vinsoyt4321@gmail.com>
2025-03-14 13:22:50 +0800

    changed file name to follow coding standard style

--- display_all_numbers_that_have_duplicates.py
+++ display_all_numbers_that_have_duplicates.py
@@ -7,4 +7,4 @@ num_list = [int(input(f"Enter number {i + 1}: ")) for i in range(10)]
 duplicate_num = {num for num in num_list if num_list.count(num) > 1}
 
 # Print the numbers.
-print(f"The duplicate numbers are: {duplicate_num}.")
+print(f"The duplicated numbers are: {duplicate_num}.")
